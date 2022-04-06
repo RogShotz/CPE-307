22b7c72867fa1432218dcd24149a194c5b8552bb
RogShotz <lukeroweprivate@gmail.com>
2022-04-06 13:16:16 -0700

    finish 6.3

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -35,7 +35,7 @@ app.get('/users/:id', (req, res) => {
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.status(201).end();
+    res.status(201).end().json(userToAdd);
 });
 
 function addUser(user){
